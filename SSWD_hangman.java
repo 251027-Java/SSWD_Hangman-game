4de27def26ff3527f674d0973e6895f83878847b
Donovan Rodgers <donovanrodgers38@gmail.com>
2025-10-29 14:32:10 -0500

    Objects and guesss word created

--- SSWD_hangman.java
+++ SSWD_hangman.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 class SSWD_Hangman{
 
+    public String guessWord(String userIn){
+
+        return "";
+    }
+
     public static void main(String[] args){
-        
+
+        String mainWordArray = "";
+        String workingWordArray ="";
+        Scanner scanin = new Scanner(System.in);
+        int tryCount = 0;
+
+
     }
 }
