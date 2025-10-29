cd6928e5455c8d44baa3dcfca57520706a1d9ce0
Shajira Guzman <shajiraguzman@gmail.com>
2025-10-29 16:01:07 -0500

    Guess a letter logic and printing game status

--- SSWD_hangman.java
+++ SSWD_hangman.java
@@ -1,26 +1,62 @@
+import java.nio.file.FileSystemNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 class SSWD_Hangman{
+
+   static GameSession hangman;
+   static Set<Character> mainWord = new HashSet<>();
+   static Set<Character> workingWord = new HashSet<>();
 
     public String guessWord(String userIn){
 
         return "";
     }
 
-    public String guessALetter(char c) {
-        return "hello";
+    public static boolean guessALetter(char c) {
+
+        if (!mainWord.contains(c)) {
+            hangman.decrementGuesses();
+            System.out.println("Wrong guess!");
+            return false;
+        }
+
+        workingWord.add(c);
+        if (mainWord.equals(workingWord))
+            System.out.println("You Win!");
+
+        displayGameState();
+        return true;
     }
 
     public static void main(String[] args){
-        GameSession hangman = new GameSession("hangman", 6);
-
+        hangman = new GameSession("hangman", 6);
+        setMainWordSet(hangman.getWord());
         String mainWordArray = "";
         String workingWordArray ="";
         Scanner scanin = new Scanner(System.in);
         int tryCount = 0;
 
 
+    }
 
+    public static void displayGameState() {
+        String word = hangman.getWord();
+        for (int i = 0; i < word.length(); i++) {
+            if (workingWord.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+                continue;
+            }
+            System.out.print("_");
+        }
+        System.out.println();
+    }
 
+    private static void setMainWordSet(String word) {
+        System.out.println("Current status");
+        for (int i = 0; i < word.length(); i++) {
+            mainWord.add(word.charAt(i));
+        }
     }
 }
