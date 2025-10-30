ee3a0e53d318c60af6efaa2d70b560781f2db2e1
Chilllnote <donovanRodgers38@gmail.com>
2025-10-30 15:29:41 -0500

    Update Cards.java

--- Project1_BlackJack/Cards.java
+++ Project1_BlackJack/Cards.java
@@ -4,6 +4,7 @@ public class Cards {
 
     // ========Cards class==========
     // Purpose ==  Cards class holds the cards for the entire game. Shuffle will only execute after game ends
+    // Object == Cards class will use an ArrayList object for it's returned values
     // Cards() - constructor
     // drawCards(int i) decrements the given number of cards in the deck and returns them
     // shuffle(Cards playercards, Cards dealercards) adds back all cards from player and house and shuffles them
