d579d9e0b050723a851a0793a222bd42dc05c6b5
Chilllnote <donovanRodgers38@gmail.com>
2025-10-30 15:30:36 -0500

    Clarify comments in Cards class
    Updated comments in the Cards class to clarify constructor and shuffle method functionality.

--- Project1_BlackJack/Cards.java
+++ Project1_BlackJack/Cards.java
@@ -5,9 +5,9 @@ public class Cards {
     // ========Cards class==========
     // Purpose ==  Cards class holds the cards for the entire game. Shuffle will only execute after game ends
     // Object == Cards class will use an ArrayList object for it's returned values
-    // Cards() - constructor
+    // Cards() - constructor. Initializes a full deck of cards each time the class is made
     // drawCards(int i) decrements the given number of cards in the deck and returns them
-    // shuffle(Cards playercards, Cards dealercards) adds back all cards from player and house and shuffles them
+    // shuffle(Cards playercards, Cards dealercards) adds back all cards from player and house and shuffles them. MUST REMOVE CARDS FROM PLAYERS AND HOUSE BEFORE SHUFFLE
     // showCards(Cards playercards, Cards dealerCards) takes in cards object from player and Dealer and returns what is had
     // =======================
 }
